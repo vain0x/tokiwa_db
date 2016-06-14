8774f865971044209fcdfced6c601eda19f87ee5
vain0 <id_vain@yahoo.co.jp>
2016-06-15 08:59:31 +0900

    WIP

---
+++ TokiwaDb.Core.Test/DatabaseTest.fs
@@ -0,0 +1,17 @@
+﻿namespace TokiwaDb.Core.Test
+
+open Persimmon
+open Persimmon.Syntax.UseTestNameByReflection
+open TokiwaDb.Core
+
+module DatabaseTest =
+  open TokiwaDb.Core.Test.TableTest.TestData
+
+  let createTest =
+    test {
+      let testDb =
+        StreamDatabase(@"testDb")
+      let persons = persons ()
+      testDb.CreateTable()
+    }
+
