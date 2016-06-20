ff6c614c3145bc95d35ad3d7dce45880fef98875
vain0 <id_vain@yahoo.co.jp>
2016-06-20 21:47:03 +0900

    Add FixedLengthTupleSerializer

--- TokiwaDb.Core/Serializer.fs
+++ TokiwaDb.Core/Serializer.fs
@@ -1,11 +1,47 @@
 ﻿namespace TokiwaDb.Core
 
+open Microsoft.FSharp.Reflection
+
+type INongenericFixedLengthSerializer =
+  abstract member Length: int64
+  abstract member Serialize: obj -> array<byte>
+  abstract member Deserialize: array<byte> -> obj
+
 /// A serializer which can serialize a value to a byte array with fixed length;
 /// and can deserialize the byte array to the original value.
 type [<AbstractClass>] FixedLengthSerializer<'x>() =
   abstract member Serialize: 'x -> array<byte>
   abstract member Deserialize: array<byte> -> 'x
   abstract member Length: int64
+
+  interface INongenericFixedLengthSerializer with
+    override this.Length = this.Length
+
+    override this.Serialize(value: obj) =
+      this.Serialize(value :?> 'x)
+
+    override this.Deserialize(data) =
+      this.Deserialize(data) :> obj
+
+module FixedLengthSerializerDetail =
+  let serializeSequence (xs: array<obj>) (serializers: array<INongenericFixedLengthSerializer>) =
+    Array.zip xs serializers
+    |> Array.collect (fun (field, serializer) -> serializer.Serialize(field))
+
+  let deserializeSequence
+      (data: array<byte>) (i: int)
+      (serializers: array<INongenericFixedLengthSerializer>)
+    =
+    let p = ref i
+    in
+      [|
+        for serializer in serializers do
+          let q = (! p) + (int serializer.Length)
+          yield serializer.Deserialize(data.[(! p)..(q - 1)])
+          p := q
+      |]
+
+open FixedLengthSerializerDetail
 
 type FixedLengthArraySerializer<'x>
   ( _serializer: FixedLengthSerializer<'x>
@@ -35,26 +71,30 @@ type FixedLengthArraySerializer<'x>
         yield _serializer.Deserialize(data.[p..q])
     |]
 
+type FixedLengthTupleSerializer<'t>(_serializers: array<INongenericFixedLengthSerializer>) =
+  inherit FixedLengthSerializer<'t>()
+
+  static do
+    assert (FSharpType.IsTuple(typeof<'t>))
+
+  do
+    assert (FSharpType.GetTupleElements(typeof<'t>).Length = _serializers.Length)
+
+  override this.Length =
+    _serializers |> Array.sumBy (fun fls -> fls.Length)
+
+  override this.Serialize(t: 't) =
+    serializeSequence (FSharpValue.GetTupleFields(t)) _serializers
+
+  override this.Deserialize(data) =
+    let elements = deserializeSequence data 0 _serializers
+    in FSharpValue.MakeTuple(elements, typeof<'t>) :?> 't
+
 type FixedLengthDoubleSerializer<'x0, 'x1>
   ( _serializer0: FixedLengthSerializer<'x0>
   , _serializer1: FixedLengthSerializer<'x1>
   ) =
-  inherit FixedLengthSerializer<'x0 * 'x1>()
-
-  override this.Length =
-    _serializer0.Length + _serializer1.Length
-
-  override this.Serialize((x0, x1)) =
-    [|
-      yield! _serializer0.Serialize(x0)
-      yield! _serializer1.Serialize(x1)
-    |]
-
-  override this.Deserialize(data) =
-    assert (data.LongLength = this.Length)
-    let x0 = _serializer0.Deserialize(data.[..(_serializer0.Length - 1L |> int)])
-    let x1 = _serializer1.Deserialize(data.[(_serializer0.Length |> int)..])
-    in (x0, x1)
+  inherit FixedLengthTupleSerializer<'x0 * 'x1>([| _serializer0; _serializer1 |])
 
 type FixedLengthQuadrupleSerializer<'x0, 'x1, 'x2, 'x3>
   ( _serializer0: FixedLengthSerializer<'x0>
@@ -62,19 +102,4 @@ type FixedLengthQuadrupleSerializer<'x0, 'x1, 'x2, 'x3>
   , _serializer2: FixedLengthSerializer<'x2>
   , _serializer3: FixedLengthSerializer<'x3>
   ) =
-  inherit FixedLengthSerializer<'x0 * 'x1 * 'x2 * 'x3>()
-
-  let _serializer =
-    FixedLengthDoubleSerializer(_serializer0,
-      FixedLengthDoubleSerializer(_serializer1,
-        FixedLengthDoubleSerializer(_serializer2, _serializer3)))
-
-  override this.Length =
-    _serializer.Length
-
-  override this.Serialize((x0, x1, x2, x3)) =
-    _serializer.Serialize(x0, (x1, (x2, x3)))
-
-  override this.Deserialize(data) =
-    let (x0, (x1, (x2, x3))) = _serializer.Deserialize(data)
-    in (x0, x1, x2, x3)
+  inherit FixedLengthTupleSerializer<'x0 * 'x1 * 'x2 * 'x3>([| _serializer0; _serializer1; _serializer2; _serializer3 |])
