c337d160c9ad14717dd77e1131bf8381eabe0ff0
vain0 <id_vain@yahoo.co.jp>
2016-06-20 17:51:49 +0900

    Add Serializer.fs

---
+++ TokiwaDb.Core/Serializer.fs
@@ -0,0 +1,8 @@
+﻿namespace TokiwaDb.Core
+
+/// A serializer which can serialize a value to a byte array with fixed length;
+/// and can deserialize the byte array to the original value.
+type [<AbstractClass>] FixedLengthSerializer<'x>() =
+  abstract member Serialize: 'x -> array<byte>
+  abstract member Deserialize: array<byte> -> 'x
+  abstract member Length: int64
