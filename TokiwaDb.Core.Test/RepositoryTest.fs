f964f073aa41eeb62c24cfc7ae75370ef100a1da
vain0 <id_vain@yahoo.co.jp>
2016-06-25 11:05:11 +0900

    Add Repository, MemoryRepository, FileSystemRepository

---
+++ TokiwaDb.Core.Test/RepositoryTest.fs
@@ -0,0 +1,28 @@
+﻿namespace TokiwaDb.Core.Test
+
+open Persimmon
+open Persimmon.Syntax.UseTestNameByReflection
+open TokiwaDb.Core
+
+module RepositoryTest =
+  let repositoryTest (repo: Repository) =
+    [
+      test {
+        let _ = repo.AddSubrepository("sub")
+        do! repo.TryFindSubrepository("sub")
+          |> Option.map (fun sub -> sub.Name)
+          |> assertEquals (Some "sub")
+      }
+      test {
+        let _ = repo.Add("x0.x")
+        let _ = repo.Add("x1.x")
+        let _ = repo.Add("y0.y")
+        do! repo.TryFind("x0.x") |> Option.isSome |> assertPred
+        do! repo.TryFind("____") |> assertEquals None
+        do! repo.FindManyBySuffix(".x") |> Seq.length |> assertEquals 2
+      }
+    ]
+
+  let memoryRepositoryTest =
+    MemoryRepository("mem_repo")
+    |> repositoryTest
---
+++ TokiwaDb.Core/Repository.fs
@@ -0,0 +1,99 @@
+﻿namespace TokiwaDb.Core
+
+open System.Collections.Generic
+open System.IO
+
+type [<AbstractClass>] Repository() =
+  abstract member Name: string
+  abstract member FullName: string
+
+  abstract member AddSubrepository: string -> Repository
+  abstract member TryFindSubrepository: string -> option<Repository>
+
+  abstract member Add: string -> StreamSource
+  abstract member TryFind: string -> option<StreamSource>
+  abstract member FindManyBySuffix: string -> seq<StreamSource>
+
+type MemoryRepository(_name: string) =
+  inherit Repository()
+
+  let _repositories = Dictionary<string, Repository>()
+
+  let _items = Dictionary<string, StreamSource>()
+
+  override this.Name = _name
+
+  override this.FullName = _name
+
+  override this.AddSubrepository(name) =
+    let repo      = MemoryRepository(name) :> Repository
+    let _         = _repositories.Add(name, repo)
+    in repo
+
+  override this.TryFindSubrepository(name) =
+    _repositories.TryGetValue(name) |> Option.ofPair
+
+  override this.Add(name) =
+    let item      = MemoryStreamSource() :> StreamSource
+    let _         = _items.Add(name, item)
+    in item
+
+  override this.TryFind(name) =
+    _items.TryGetValue(name) |> Option.ofPair
+
+  override this.FindManyBySuffix(suffix) =
+    _items |> Seq.choose (fun (KeyValue (name, item)) ->
+      if name.EndsWith(suffix)
+      then Some item
+      else None
+      )
+
+type FileSystemRepository(_path: string) =
+  inherit Repository()
+
+  let _dir = DirectoryInfo(_path)
+
+  let _subdir name = DirectoryInfo(Path.Combine(_dir.FullName, name))
+
+  let _subrepo (subdir: DirectoryInfo) =
+    FileSystemRepository(subdir.FullName) :> Repository
+
+  let _subfile name = FileInfo(Path.Combine(_dir.FullName, name))
+
+  let _subitem (subfile: FileInfo) =
+    FileStreamSource(subfile) :> StreamSource
+
+  override this.Name = _dir.Name
+
+  override this.FullName = _dir.FullName
+
+  override this.AddSubrepository(name) =
+    let subdir      = _subdir name
+    let ()          = subdir.Create()
+    let subrepo     = _subrepo subdir
+    in subrepo
+
+  override this.TryFindSubrepository(name) =
+    _dir.GetDirectories(name)
+    |> Array.tryHead
+    |> Option.map _subrepo
+
+  override this.Add(name) =
+    let subfile     = _subfile name
+    let ()          =
+      if File.Exists(subfile.FullName) |> not then
+        subfile |> FileInfo.createNew
+    in _subitem subfile
+
+  override this.TryFind(name) =
+    _dir.GetFiles(name)
+    |> Array.tryHead
+    |> Option.map _subitem
+
+  override this.FindManyBySuffix(suffix) =
+    _dir.GetFiles()
+    |> Seq.choose (fun subfile ->
+      if subfile.Name.EndsWith(suffix)
+      then subfile |> _subitem |> Some
+      else None
+      )
