9e3df642764dfee62c3492c6569b9a35fcb73ced
vain0 <id_vain@yahoo.co.jp>
2016-07-11 22:13:20 +0900

    Test unique index set by db config

--- TokiwaDb.CodeFirst.Test/DbConfigTest.fs
+++ TokiwaDb.CodeFirst.Test/DbConfigTest.fs
@@ -7,10 +7,23 @@ open TokiwaDb.CodeFirst
 open TokiwaDb.CodeFirst.Detail
 
 module DbConfigTest =
-  let normalTest =
+  let dbConfig () =
+    let dbConfig = DbConfig<TestDbContext>()
+    dbConfig.Add<Person>(UniqueIndex.Of<Person>([| "Name"; "Age" |]))
+    dbConfig
+
+  let openTest =
     test {
-      let dbConfig = DbConfig<TestDbContext>()
-      dbConfig.Add<Person>(UniqueIndex.Of<Person>([| "Name"; "Age" |]))
+      let dbConfig = dbConfig ()
       dbConfig.OpenMemory("") |> ignore
+      return ()
+    }
+
+  let uniqueIndexTest =
+    test {
+      let dbConfig = dbConfig ()
+      let db = dbConfig.OpenMemory("")
+      db.Persons.Insert(Person(Name = "Miku", Age = 16L))
+      let _ = trap { it (db.Persons.Insert(Person(Name = "Miku", Age = 18L))) }
       return ()
     }
