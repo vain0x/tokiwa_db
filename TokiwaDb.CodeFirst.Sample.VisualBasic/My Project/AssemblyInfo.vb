3eda4d5a2db89d41a54e077337a46fc5abd2099e
vain0 <id_vain@yahoo.co.jp>
2016-07-25 20:30:11 +0900

    Add TokiwaDb.CodeFirst.Sample.VisualBasic

---
+++ TokiwaDb.CodeFirst.Sample.VisualBasic/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する情報は、次の属性のセットを通して制御されます。
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("TokiwaDb.CodeFirst.Sample.VisualBasic")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("TokiwaDb.CodeFirst.Sample.VisualBasic")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+<Assembly: Guid("be327244-088e-4c33-830d-8261ea5cba95")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      Revision
+'
+' すべての値を指定するか、以下のように '*' を使用してビルドおよびリビジョン番号を
+' 既定値にすることができます:
+' <アセンブリ: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
